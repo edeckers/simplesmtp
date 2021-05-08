c88f9f0e14cd198f233c7efa369b9c1ea09fe09a
Ely Deckers <e.deckers@gmail.com>
2021-05-08 15:01:54 +0200

    Add simple test client

---
+++ bin/run-test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PORT=${PORT:-9999}
+
+echo "
+ehlo domain.com
+helo domain.com
+mail from: mailbox@domain.com
+rcpt to: ely@infi.nl
+data
+hello world
+
+.
+
+quit" | nc localhost ${PORT}
+
